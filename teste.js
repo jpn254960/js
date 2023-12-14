9538abbda95229168abd0320aa8b0d137d0c298d
jpn254960 <111208020+jpn254960@users.noreply.github.com>
2023-12-14 03:59:34 -0300

    fff
    ffff

--- teste.js
+++ teste.js
@@ -4,7 +4,7 @@ var nome = String(document.getElementById('nome').value)
 var senha= String(document.getElementById('senha').value)
 if (nome=='julia' && senha=='10/03/2023'){
     open('video.html')
+}else{
+    open('erro.html')
 }
-
-
 }
